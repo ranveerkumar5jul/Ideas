0875644b8d54f80789e4e2aa9e490669be958de4
Ranveer Kumar <ranveerkumar5jul@gmail.com>
2015-12-02 11:16:57 +0530

    Moved test data setup from implementation class to test class.

--- src/com/example/flightbooking/FlightBookingClient.java
+++ src/com/example/flightbooking/FlightBookingClient.java
@@ -1,80 +1,17 @@
 package com.example.flightbooking;
 
+import static com.example.flightbooking.FlightBookingConstants.DEFAULT_FREQUENCY;
+
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import static com.example.flightbooking.FlightBookingConstants.*;
-
-import com.example.flightbooking.Path;
 
 public class FlightBookingClient {
+
 	Map<Path, List<Path>> airportConnectivityMap = null;
-	List<Path> airportList = new ArrayList<Path>();
 
-	public FlightBookingClient() {
-		populateAirportList();
-		createAirportConnectivityMap();
-	}
-
-	/*
-	 * Below method is to populate a map with key as path object and value as
-	 * list of path object where path will have source, destination and
-	 * available flight frequency.
-	 */
-
-	public void createAirportConnectivityMap() {
-		airportConnectivityMap = new HashMap<Path, List<Path>>();
-		for (Path edge : airportList) {
-
-			List<Path> destList = airportConnectivityMap.get(edge);
-			if (destList == null) {
-				destList = new ArrayList<Path>();
-			}
-
-			destList.add(edge);
-			airportConnectivityMap.put(edge, destList);
-
-		}
-	}
-
-	/*
-	 * Below method is to populate a list of Path object where path will have
-	 * source, destination and available flight frequency.
-	 */
-
-	public void populateAirportList() {
-		airportList.add(new Path(AIRPORT_A, AIRPORT_F, 1.0));
-
-		airportList.add(new Path(AIRPORT_B, AIRPORT_A, 1.0));
-		airportList.add(new Path(AIRPORT_B, AIRPORT_F, 2.0));
-
-		airportList.add(new Path(AIRPORT_C, AIRPORT_D, 1.0));
-		airportList.add(new Path(AIRPORT_C, AIRPORT_E, 5.0));
-		airportList.add(new Path(AIRPORT_C, AIRPORT_E, 4.0));
-		airportList.add(new Path(AIRPORT_C, AIRPORT_E, 2.0));
-		airportList.add(new Path(AIRPORT_C, AIRPORT_B, 3.0));
-		airportList.add(new Path(AIRPORT_C, AIRPORT_B, 5.0));
-		airportList.add(new Path(AIRPORT_C, AIRPORT_B, 6.0));
-		airportList.add(new Path(AIRPORT_C, AIRPORT_A, 6.0));
-		airportList.add(new Path(AIRPORT_C, AIRPORT_A, 7.0));
-		airportList.add(new Path(AIRPORT_C, AIRPORT_A, 4.0));
-		airportList.add(new Path(AIRPORT_C, AIRPORT_F, 7.0));
-		airportList.add(new Path(AIRPORT_C, AIRPORT_F, 8.0));
-		airportList.add(new Path(AIRPORT_C, AIRPORT_F, 5.0));
-
-		airportList.add(new Path(AIRPORT_D, AIRPORT_E, 1.0));
-		airportList.add(new Path(AIRPORT_D, AIRPORT_B, 2.0));
-		airportList.add(new Path(AIRPORT_D, AIRPORT_A, 3.0));
-		airportList.add(new Path(AIRPORT_D, AIRPORT_F, 4.0));
-
-		airportList.add(new Path(AIRPORT_E, AIRPORT_B, 1.0));
-		airportList.add(new Path(AIRPORT_E, AIRPORT_A, 2.0));
-		airportList.add(new Path(AIRPORT_E, AIRPORT_F, 3.0));
-
-		airportList.add(new Path(AIRPORT_H, AIRPORT_B, 1.0));
-		airportList.add(new Path(AIRPORT_H, AIRPORT_A, 2.0));
-		airportList.add(new Path(AIRPORT_H, AIRPORT_F, 3.0));
+	public FlightBookingClient(Map<Path, List<Path>> airportConnectivityMap) {
+		this.airportConnectivityMap = airportConnectivityMap;
 	}
 
 	public boolean checkIfRouteExists(String source, String destination) {
@@ -88,12 +25,12 @@ public class FlightBookingClient {
 
 	public Double getShortestPath(String source, String destination) {
 		boolean checkRouteExist = false;
-		Double shortestWeight = new Double(99.0);
+		Double shortestWeight = new Double(999.0);
 		checkRouteExist = checkIfRouteExists(source, destination);
 		if (checkRouteExist) {
 			List<Path> destinationList = airportConnectivityMap.get(new Path(source, destination, DEFAULT_FREQUENCY));
-			for (Path edge : destinationList) {
-				shortestWeight = edge.getFrequency() < shortestWeight ? edge.getFrequency() : shortestWeight;
+			for (Path path : destinationList) {
+				shortestWeight = path.getFrequency() < shortestWeight ? path.getFrequency() : shortestWeight;
 			}
 		}
 		return shortestWeight;
--- src/com/example/flightbooking/FlightBookingClientTest.java
+++ src/com/example/flightbooking/FlightBookingClientTest.java
@@ -3,31 +3,96 @@
  */
 package com.example.flightbooking;
 
+import static com.example.flightbooking.FlightBookingConstants.AIRPORT_A;
+import static com.example.flightbooking.FlightBookingConstants.AIRPORT_B;
+import static com.example.flightbooking.FlightBookingConstants.AIRPORT_C;
+import static com.example.flightbooking.FlightBookingConstants.AIRPORT_D;
+import static com.example.flightbooking.FlightBookingConstants.AIRPORT_E;
+import static com.example.flightbooking.FlightBookingConstants.AIRPORT_F;
+import static com.example.flightbooking.FlightBookingConstants.AIRPORT_H;
+import static com.example.flightbooking.FlightBookingConstants.DEFAULT_FREQUENCY;
 import static org.junit.Assert.assertEquals;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 public class FlightBookingClientTest {
-	
-	@Test
-	public void checkIfRouteExistsTest(){
-		FlightBookingClient fbc = new FlightBookingClient();
-		boolean checkRoutes = fbc.checkIfRouteExists(FlightBookingConstants.AIRPORT_C, FlightBookingConstants.AIRPORT_B);
-		assertEquals(true,checkRoutes);
+	static Map<Path, List<Path>> airportConnectivityMap = new HashMap<Path, List<Path>>();
+
+	@BeforeClass
+	public static void setUp() {
+		addRoute(AIRPORT_A, AIRPORT_F, 1.0);
+		addRoute(AIRPORT_B, AIRPORT_A, 1.0);
+		addRoute(AIRPORT_B, AIRPORT_F, 2.0);
+
+		addRoute(AIRPORT_C, AIRPORT_D, 1.0);
+		addRoute(AIRPORT_C, AIRPORT_E, 5.0);
+		addRoute(AIRPORT_C, AIRPORT_E, 4.0);
+		addRoute(AIRPORT_C, AIRPORT_E, 2.0);
+		addRoute(AIRPORT_C, AIRPORT_B, 3.0);
+		addRoute(AIRPORT_C, AIRPORT_B, 5.0);
+		addRoute(AIRPORT_C, AIRPORT_B, 6.0);
+		addRoute(AIRPORT_C, AIRPORT_A, 6.0);
+		addRoute(AIRPORT_C, AIRPORT_A, 7.0);
+		addRoute(AIRPORT_C, AIRPORT_A, 4.0);
+		addRoute(AIRPORT_C, AIRPORT_F, 7.0);
+		addRoute(AIRPORT_C, AIRPORT_F, 8.0);
+		addRoute(AIRPORT_C, AIRPORT_F, 5.0);
+
+		addRoute(AIRPORT_D, AIRPORT_E, 1.0);
+		addRoute(AIRPORT_D, AIRPORT_B, 2.0);
+		addRoute(AIRPORT_D, AIRPORT_A, 3.0);
+		addRoute(AIRPORT_D, AIRPORT_F, 4.0);
+
+		addRoute(AIRPORT_E, AIRPORT_B, 1.0);
+		addRoute(AIRPORT_E, AIRPORT_A, 2.0);
+		addRoute(AIRPORT_E, AIRPORT_F, 3.0);
+
+		addRoute(AIRPORT_H, AIRPORT_B, 1.0);
+		addRoute(AIRPORT_H, AIRPORT_A, 2.0);
+		addRoute(AIRPORT_H, AIRPORT_F, 3.0);
 	}
-	
-	@Test
-	public void checkIfRouteNotExistsTest(){
-		FlightBookingClient fbc = new FlightBookingClient();
-		boolean checkRoutes = fbc.checkIfRouteExists(FlightBookingConstants.AIRPORT_H, FlightBookingConstants.AIRPORT_E);
-		assertEquals(false,checkRoutes);
+
+	private static void addRoute(String source, String destination, double flightFrequency) {
+		Path path = new Path(source, destination, flightFrequency);
+		Path getPath = new Path(source, destination, DEFAULT_FREQUENCY);
+		List<Path> destList = airportConnectivityMap.get(getPath);
+		if (destList == null) {
+			destList = new ArrayList<Path>();
+			destList.add(path);
+		} else {
+			destList.add(path);
+		}
+		airportConnectivityMap.put(getPath, destList);
+
 	}
-	
+
 	@Test
-	public void getShortestPathTest(){
-		FlightBookingClient fbc = new FlightBookingClient();
+	public void checkIfRouteExistsTest() {
+		FlightBookingClient fbc = new FlightBookingClient(airportConnectivityMap);
+		boolean checkRoutes = fbc.checkIfRouteExists(FlightBookingConstants.AIRPORT_C,
+				FlightBookingConstants.AIRPORT_B);
+		assertEquals(true, checkRoutes);
+	}
+
+	@Test
+	public void checkIfRouteNotExistsTest() {
+		FlightBookingClient fbc = new FlightBookingClient(airportConnectivityMap);
+		boolean checkRoutes = fbc.checkIfRouteExists(FlightBookingConstants.AIRPORT_H,
+				FlightBookingConstants.AIRPORT_E);
+		assertEquals(false, checkRoutes);
+	}
+
+	@Test
+	public void getShortestPathTest() {
+		FlightBookingClient fbc = new FlightBookingClient(airportConnectivityMap);
 		Double shortestWeight = fbc.getShortestPath(FlightBookingConstants.AIRPORT_C, FlightBookingConstants.AIRPORT_B);
-		assertEquals(new Double(3.0),shortestWeight);
+		assertEquals(new Double(3.0), shortestWeight);
 	}
-		
+
 }
